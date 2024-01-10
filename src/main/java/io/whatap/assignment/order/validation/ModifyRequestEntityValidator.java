4292a709d397ec2538c3298c72b14aa1dbc67dc0
INSU <tonuto@naver.com>
2024-01-11 00:53:24 +0900

    버그 수정

--- src/main/java/io/whatap/assignment/order/validation/ModifyRequestEntityValidator.java
+++ src/main/java/io/whatap/assignment/order/validation/ModifyRequestEntityValidator.java
@@ -24,7 +24,9 @@ public class ModifyRequestEntityValidator implements RequestEntityValidator<Orde
     List<ValidationError> errorList = new ArrayList<>();
 
     List<ProductOrderRequest> productModifyList = request.getProductModifyList();
-    if (!productModifyList.isEmpty()) {
+    if (!Objects.isNull(productModifyList)) {
+      errorList.add(ValidationError.builder().field("productModifyList").message(IS_NULL).build());
+    } else {
       int index = 0;
       for (ProductOrderRequest p : productModifyList) {
         if (Objects.isNull(p.getId())) {
